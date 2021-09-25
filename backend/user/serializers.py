59ea93d2872d58930952d5f2005032a6d12c4fe6
Kamil-Sabbagh <69242886+Kamil-Sabbagh@users.noreply.github.com>
2021-09-26 00:45:31 +0300

    Delete serializers.py

--- backend/user/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password', 'user_type')
-
