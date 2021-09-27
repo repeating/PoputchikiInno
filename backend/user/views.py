db5324e479c9666c00a70a055f7a15a82e273ee7
Karam19 <karam.shbeb1@hotmail.com>
2021-09-27 23:50:26 +0300

    fix login

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,4 +1,3 @@
-from django import http
 from django.http import HttpResponseRedirect, response
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
@@ -6,15 +5,24 @@ from django.views import generic
 # Create your views here.
 from rest_framework import viewsets  # add this
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
-from django.http import HttpResponse
+#from .serializers import UserSerializer
+from django.http import HttpResponse, JsonResponse
 from django.views import generic
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.models import User as A_user
 from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 from django.contrib.auth import get_user_model
+from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .forms import ProfileRegisterForm , ProfileLogInForm
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import parser_classes
+import json
+from .models import Profile
+import ast
 
 
 User = get_user_model()
@@ -25,31 +33,36 @@ class LogOutView(generic.DetailView):
 
 
 def home(request):
-    return render(request, 'user/home.html', {'data' : 'data'})
+    content = {
+            'status' : 'request was permitted'
+        }
+    return render(request, 'user/home.html', content)
 
+
+@csrf_exempt
 def login(request):
     if request.method == 'POST':
-        form = ProfileLogInForm(data=request.POST)
-        if form.is_valid():
-            return HttpResponse({'message': 'valid'})
+        data = ast.literal_eval(request.body.decode())
+        users = User.objects.all().filter(username=data['username'])
+        if len(users) == 1 and users[0].password == data['password']:
+            return JsonResponse({'token': data['username']})
         else:
-            return HttpResponse({'invalid': 'invalid'})
+            return JsonResponse({'token': ''})
     else:
         form = ProfileLogInForm()
     return render(request, 'user/login.html', {'form' : form})
 
 def register(request):
     if request.method == 'POST':
-        form = ProfileRegisterForm(request.POST)
+        form = ProfileRegisterForm(request.body)
         if form.is_valid():
             form.save()
             messages.success(request, f'Your account has been created! You are now able to log in')
-            return HttpResponse ( {'token': form.user_name} )
-        else :
-            return HttpResponse({'token': ''})
+            #return redirect('../../user/home')
+            return JsonResponse({'token': request.body['username']})
     else :
         form = ProfileRegisterForm()
-    return render(request , 'user/register.html' , {'form': form } )
+    return render(request , 'user/register.html' , {'form': form})
 
 
 def logout(request, user_id):
